859684d1becd3b4dc960a69ab3de3850c55040af
haroon-developerr <haroon51166@gmail.com>
2025-05-01 01:31:32 +0400

    Initial commit

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ-1_4mzIXCtO_zuaoOtnDY7Z_3u_6NFs",
+  authDomain: "postream-2fa6b.firebaseapp.com",
+  projectId: "postream-2fa6b",
+  storageBucket: "postream-2fa6b.firebasestorage.app",
+  messagingSenderId: "230039290042",
+  appId: "1:230039290042:web:a284f98aa00c8d6707c283"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase
+export default app;
